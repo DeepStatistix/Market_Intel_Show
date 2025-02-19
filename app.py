70a12773be195d0b5bc8a84b925e5df89c9c23fe
akibmir90 <maaqib90@gmail.com>
2025-02-19 17:21:00 +0530

    app modified

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import matplotlib.dates as mdates
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
 # Flask and local imports
 from flask import Flask, render_template, request, jsonify
 from werkzeug.exceptions import BadRequest
